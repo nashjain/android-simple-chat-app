b363c9b23129ec74d9cef1cb8c9a808ce11116fd
Kshama Singh <kshamasingh@Kshamas-MacBook-Pro.local>
2016-07-06 12:57:32 +0530

    MainActivity test case

---
+++ app/src/androidTest/java/com/agilefaqs/chatapp/MainActivityTest.java
@@ -0,0 +1,38 @@
+package com.agilefaqs.chatapp;
+
+/**
+ * Created by kshamasingh on 06/07/16.
+ */
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+
+import com.confengine.chatapp.R;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private MainActivity mainActivity;
+    private Button sendButton;
+    private String buttonString;
+
+    public MainActivityTest() {
+        super("com.agilefaqs.android.first", MainActivity.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        mainActivity = this.getActivity();
+        sendButton = (Button) mainActivity.findViewById
+                (R.id.sendButton);
+        buttonString = "Send";
+    }
+
+    public void testCasesPrecondition() {
+        assertNotNull(sendButton);
+    }
+
+    public void buttonTextVerification() {
+        assertEquals(buttonString, (String) sendButton.getText());
+    }
+
+}
