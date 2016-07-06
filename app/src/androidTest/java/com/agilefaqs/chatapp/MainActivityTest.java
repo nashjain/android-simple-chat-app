d17462563830a313021e25038a552454396816e6
Sidharth Guglani <sidharth.guglani@gmail.com>
2016-07-06 13:37:14 +0530

    added send btn test cases

--- app/src/androidTest/java/com/agilefaqs/chatapp/MainActivityTest.java
+++ app/src/androidTest/java/com/agilefaqs/chatapp/MainActivityTest.java
@@ -1,7 +1,11 @@
 package com.agilefaqs.chatapp;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.confengine.chatapp.R;
 
 /**
  * Created by sidharth on 06/07/16.
@@ -10,6 +14,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 
     private MainActivity mActivity;
     private EditText mView;
+    private Button sendBtn;
     private String resourceString;
 
     public MainActivityTest() {
@@ -20,6 +25,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
         super.setUp();
         mActivity = this.getActivity();
         mView = (EditText) mActivity.findViewById(com.confengine.chatapp.R.id.messageInput);
+        sendBtn = (Button) mActivity.findViewById(R.id.sendButton);
         resourceString = "Enter message here";
     }
 
@@ -28,6 +34,18 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     }
 
     public void testText() {
-        assertEquals(resourceString,(String) mView.getText().toString());
+        assertEquals(resourceString, mView.getHint().toString());
+    }
+
+    @UiThreadTest
+    public void testSendButtonDisabledIfMessageInputIsEmpty() {
+        mView.setText("");
+        assertEquals(false, sendBtn.isEnabled());
+    }
+
+    @UiThreadTest
+    public void testSendButtonEnabledIfMessageInputIsNotEmpty() {
+        mView.setText("abc");
+        assertEquals(true, sendBtn.isEnabled());
     }
 }
